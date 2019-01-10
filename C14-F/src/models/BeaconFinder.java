48e2d69d50a87a8cd9f32abb8b4b91c4ba263176
Leo Snel <leo.snel@hva.nl>
2019-01-10 17:05:22 +0100

    update

--- C14-F/src/models/BeaconFinder.java
+++ C14-F/src/models/BeaconFinder.java
@@ -1,19 +1,24 @@
 package models;
 
 import lejos.hardware.Button;
-import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.lcd.Font;
-import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.SensorMode;
-import models.Driving;
-import models.Pilot;
 
 public class BeaconFinder {
-	boolean beaconFound = false;
+	private boolean beaconFound = false;
+
+	public boolean isBeaconFound() {
+		return beaconFound;
+	}
+
+	public void setBeaconFound(boolean beaconFound) {
+		this.beaconFound = beaconFound;
+	}
+
 	Driving drive;
-	private static final int MAXIMUM_RANGE_IR_SENSOR = 75; // eventueel aanpassen
+	private static final int MAXIMUM_RANGE_IR_SENSOR = 150; // de maximum range is tussen de 100~200 centimeter
+	// afhankelijk van de bron
 
 	// vanaf hier is de IR-sensorcode:
 
@@ -21,26 +26,27 @@ public class BeaconFinder {
 	SensorMode seek = ir.getSeekMode(); // activeert de Seek modus
 	float[] sample = new float[seek.sampleSize()]; // maakt array met sample informatie
 
-	while(Button.ESCAPE.isUp())
-	{
-		seek.fetchSample(sample, 0);
-		int bearing = (int) sample[0];
-		System.out.println("Bearing: " + bearing);
-		int distance = (int) sample[1];
-		System.out.println("Distance: " + distance);
+	public void findBeacon() {
+		while (Button.ESCAPE.isUp()) {
+			seek.fetchSample(sample, 0);
+			int bearing = (int) sample[0];
+			System.out.println("Bearing: " + bearing);
+			int distance = (int) sample[1];
+			System.out.println("Distance: " + distance);
 
-		if (distance > MAXIMUM_RANGE_IR_SENSOR) { // als het beacon out of range is
-			drive.roam(beaconFound); // begin met roam
-		} else if (distance < MAXIMUM_RANGE_IR_SENSOR) { // maximum range IR sensor
-			drive.straight(MAXIMUM_RANGE_IR_SENSOR); // begin met straight
+			if (distance > MAXIMUM_RANGE_IR_SENSOR) {
+				drive.roam(beaconFound); // begin met roam
+			} else if (distance < MAXIMUM_RANGE_IR_SENSOR) { // maximum range IR sensor
+				drive.straight(MAXIMUM_RANGE_IR_SENSOR); // begin met straight
+			}
+
+			if (bearing != 0) { // als het beacon niet recht voor de sensor is
+				drive.turn(bearing); // gaat de robot in de richting van het beacon roteren met "direction" graden
+			} else if (bearing == 0) { // als het beacon recht voor de sensor is
+				drive.straight(distance); // gaat de robot rechtuit rijden gedurende afstand "distance"
+			}
+
+			ir.close();
 		}
-
-		if (bearing != 0) { // als het beacon niet recht voor de sensor is
-			drive.turn(bearing); // gaat de robot in de richting van het beacon roteren met "direction" graden
-		} else if (bearing == 0) { // als het beacon recht voor de sensor is
-			drive.straight(distance); // gaat de robot rechtuit rijden gedurende afstand "distance"
-		}
-
-		ir.close();
 	}
-}}
+}
