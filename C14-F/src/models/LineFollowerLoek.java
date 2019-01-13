a23429bef0ac9114ffb59f6aae8a55bac89bf89f
L. van Vark <loek.van.vark@hva.nl>
2019-01-13 15:25:02 +0100

    Update LineFollowerLoek.java - now best experimental Class to test improvements on LineFollower. Not tested yet on Marvin.

--- C14-F/src/models/LineFollowerLoek.java
+++ C14-F/src/models/LineFollowerLoek.java
@@ -1,3 +1,5 @@
+// As of Sunday the primary experimental Class for LineFollower improvements
+
 package models;
 
 import lejos.hardware.Brick;
@@ -18,10 +20,12 @@ public class LineFollowerLoek {
 	public LineFollowerLoek() {
 		super();
 	}
+
 	/**
 	 * method to follow a line / this works best if the marvin is placed on the
 	 * border of the black line / with the line on the left of marvin
 	 */
+
 	public void followLine() {
 		// start with driving straight
 		Pilot pilot = new Pilot();
@@ -36,47 +40,39 @@ public class LineFollowerLoek {
 		String lastFetchedColor;
 		// Counter for #laps. Lap1 means 'now driving in lap 1, 0 completed'.
 		int lapCount = 0;
-		// declare array to store lapTimes
-		// TODO ??? int[] lapTime = new int[2] ??
+		// declare array to store lapTime
+		// TODO ??? int[] lapTime = n
 		// while not pressed continue
 		while (Button.DOWN.isUp()) {
-			while (lapCount <= 2) {
-				// do initial fetch from Sensor
-				sensor.fetchSample(lastFetchedValue, 0);
-				while (lastFetchedValue[0] >= .20 && lastFetchedValue[0] <= .60) { // while grey
-					drive.drive(); // drive straight
-					lastFetchedColor = "Grey";
-					System.out.println(lastFetchedColor);
-					// update fetch
-					sensor.fetchSample(lastFetchedValue, 0);
-				}
-				while (lastFetchedValue[0] > .60) { // while white
-					drive.turn(-5); // turn left
-					lastFetchedColor = "White";
-					System.out.println(lastFetchedColor);
-					// update fetch
-					sensor.fetchSample(lastFetchedValue, 0);
-				}
-				while (lastFetchedValue[0] < .20) { // while black
-					drive.turn(5); // turn right
-					lastFetchedColor = "Black";
-					System.out.println(lastFetchedColor);
-					// update fetch
-					sensor.fetchSample(lastFetchedValue, 0);
-				}
-				// TODO
-				// if (sensor measures red finish line && lapCount == 0) {
-				//	start.timer
-				//	lapCount++; 
-				// } else if (sensor measures red finish line && lapCount > 0) {
-				// 	stop.timer();
-				// 	lapTime[lapCount] = getLapTime();
-				// 	reset.timer();
-				//	lapCount++;
-			    // }
-				// 	print ("Lap "lapCount + " " + lapTime[lapCount - 1]) ??
-
+			// start fetching. Assumed it keeps fetching and updating so it will run correct while()
+			sensor.fetchSample(lastFetchedValue, 0);
+			while (lastFetchedValue[0] >= .20 && lastFetchedValue[0] <= .60) {	// while grey
+				drive.drive(); 													// drive straight
+				lastFetchedColor = "Grey";
+				System.out.println(lastFetchedColor);
 			}
+			while (lastFetchedValue[0] > .60) { // while white
+				drive.turn(-5);					// turn left
+				lastFetchedColor = "White";
+				System.out.println(lastFetchedColor);
+			}
+			while (lastFetchedValue[0] < .20) { // while black
+				drive.turn(5); 					// turn right
+				lastFetchedColor = "Black";
+				System.out.println(lastFetchedColor);
+			}
+			// TODO
+			// if (sensor measures red finish line && lapCount == 0) {
+			//	start.timer
+			//	lapCount++; 
+			// } else if (sensor measures red finish line && lapCount > 0) {
+			// 	stop.timer();
+			// 	lapTime[lapCount] = getLapTime();
+			// 	reset.timer();
+			//	lapCount++;
+			// }
+			// 	print ("Lap "lapCount + " " + lapTime[lapCount - 1]) ??
+
 		}
 	}
 }
