75141eee864c6897cbeea446d7d6e88b2f342992
Loek van Vark <loek.van.vark@hva.nl>
2019-01-14 15:25:44 +0100

    clean, use methods to fetch to reduce redundant code

--- C14-F/src/models/BeaconFinderLoek.java
+++ C14-F/src/models/BeaconFinderLoek.java
@@ -15,18 +15,14 @@ public class BeaconFinderLoek {
 	private static final int MAXIMUM_RANGE_IR_SENSOR = 150; // de maximum range is tussen de 100~200 centimeter
 	// afhankelijk van de bron
 
-	EV3IRSensor ir = new EV3IRSensor(SensorPort.S4); // activeert een nieuwe IR-sensor op poort S4
-	SensorMode seek = ir.getSeekMode(); // activeert de Seek modus
-	float[] sample = new float[seek.sampleSize()]; // maakt array met sample informatie
-	// edit Loek
-	// initialize the Pilot and Driving in a correct manner
-	Driving drive = new Driving();
-	Pilot pilot = new Pilot();
-	private boolean beaconFound = false;
-	
+	private EV3IRSensor ir = new EV3IRSensor(SensorPort.S4); // activeert een nieuwe IR-sensor op poort S4
+	private SensorMode seek = ir.getSeekMode(); // activeert de Seek modus
+	private float[] sample = new float[seek.sampleSize()]; // maakt array met sample informatie
 	private int bearing;
 	private int distance;
-
+	
+	private boolean beaconFound = false;
+	
 	public boolean isBeaconFound() {
 		return beaconFound;
 	}
@@ -39,7 +35,9 @@ public class BeaconFinderLoek {
 		super();
 	}
 
-	// vanaf hier is de IR-sensorcode:
+	/**
+	 * @return roam or drive towards beacon
+	 */
 
 	public void findBeacon() {
 		// @aut Loek: edit: instantiate Pilot and Driving
@@ -50,36 +48,33 @@ public class BeaconFinderLoek {
 			seek.fetchSample(sample, 0);
 			
 			//System.out.println("1st while. Bearing: " + bearing);
-			distance = (int) sample[1];
+			distance = fetchDistance();
 			System.out.println("1st while. Distance: " + distance);
 			while (distance < 3) { // IRSensor is not perfect, so assuming distance < 3 means Marvin reached beacon
 				if (distance > 21474836) {
 					setBeaconFound(false);
-					seek.fetchSample(sample, 0);
-					bearing = (int) sample[0];
-					distance = (int) sample[1];
-					// drive.roam(beaconFound); // start roam
+					
+					bearing = fetchBearing();
+					distance = fetchDistance();
 					System.out.println("2. Roam | Distance: " + distance);
 					System.out.println("2. Roam | Bearing: " + bearing);
 					drive.straight(30);
 					seek.fetchSample(sample, 0);
-					distance = (int) sample[1];
-				} else { // inRange --> turn and drive to beacon
+					distance = fetchDistance();
+				} else if (distance < 250) { // inRange --> turn and drive to beacon
 					setBeaconFound(true);
-					seek.fetchSample(sample, 0);
-					bearing = (int) sample[0];
-					distance = (int) sample[1];
+					bearing = fetchBearing();	
+					distance = fetchDistance();
 					System.out.println("2. inRange | Distance: " + distance);
 					System.out.println("2. inRange | Bearing: " + bearing);
 					// turn with bearing
-					drive.turn(bearing); 
-					seek.fetchSample(sample, 0);
-					bearing = (int) sample[0];
+					drive.turn(bearing); 	
+					distance = fetchDistance();	
 					// drive distance to beacon
 					drive.straight(distance);
 					// fetch another sample to test if movement was sufficient
-					seek.fetchSample(sample, 0);
-					distance = (int) sample[1];
+					distance = fetchDistance();
+					bearing = fetchBearing();
 				}
 			}
 		}
