e77db8c36205768e5d4594d0c1521da9948eb194
Leo Snel <leo.snel@hva.nl>
2019-01-14 15:53:30 +0100

    MoveBeacon liftUp/liftDown works, rotate/turn do not

--- C14-F/src/models/MoveBeacon.java
+++ C14-F/src/models/MoveBeacon.java
@@ -1,9 +1,12 @@
 package models;
+import models.Driving;
+//import models.Pilot;
 
 public class MoveBeacon {
 
 	private LiftArm arm = new LiftArm();
-	//private Driving drive = new Driving();
+	
+	private static int driveStraight = -100;
 
 	// no args constructor
 	public MoveBeacon() {
@@ -12,10 +15,15 @@ public class MoveBeacon {
 
 	// method to grab the beacon and put it down at the opposite side
 	public void grabBeacon() {
+		Driving drive = new Driving();
+//		Pilot pilot = new Pilot();
+		
 		arm.liftUp(); // lifts beacon up
-//		drive.setRotation(180); // rotates 180 degrees
-//		drive.straight(25); // drives straight for a distance of 25
 		System.out.println("lifted up");
+		drive.turn(40); // rotates 180 degrees
+		System.out.println("rotated 180 degrees");
+		drive.straight(driveStraight); // drives straight for a distance of 100 mm
+		//System.out.println("drove 100");
 		arm.liftDown(); // puts beacon down
 		System.out.println("put down");
 	}
