e1109d3ad68c448907f90fac9802ff12f1e28e54
L. van Vark <loek.van.vark@hva.nl>
2019-01-14 14:43:05 +0100

    Logically thids is Friday 1600. New prints and bit cleaner code.

--- C14-F/src/models/BeaconFinder.java
+++ C14-F/src/models/BeaconFinder.java
@@ -12,13 +12,9 @@ public class BeaconFinder {
 	private static final int MAXIMUM_RANGE_IR_SENSOR = 150; // de maximum range is tussen de 100~200 centimeter
 	// afhankelijk van de bron
 
-	EV3IRSensor ir = new EV3IRSensor(SensorPort.S4); // activeert een nieuwe IR-sensor op poort S4
-	SensorMode seek = ir.getSeekMode(); // activeert de Seek modus
-	float[] sample = new float[seek.sampleSize()]; // maakt array met sample informatie
-	// edit Loek
-	// initialize the Pilot and Driving in a correct manner
-	Driving drive = new Driving();
-	Pilot pilot = new Pilot();
+	private EV3IRSensor ir = new EV3IRSensor(SensorPort.S4); // activeert een nieuwe IR-sensor op poort S4
+	private SensorMode seek = ir.getSeekMode(); // activeert de Seek modus
+	private float[] sample = new float[seek.sampleSize()]; // maakt array met sample informatie
 	private boolean beaconFound = false;
 
 	public boolean isBeaconFound() {
@@ -33,44 +29,43 @@ public class BeaconFinder {
 		super();
 	}
 
-	// vanaf hier is de IR-sensorcode:
-
+	/**
+	 * If placed within range, will turn and drive towards
+	 */
 	public void findBeacon() {
-		// @aut Loek: edit: instantiate Pilot and Driving
+		// Instantiate Pilot and Driving
 		Pilot pilot = new Pilot();
 		Driving drive = new Driving(pilot.getPilot());
 
 		while (Button.ESCAPE.isUp()) {
 			seek.fetchSample(sample, 0);
 			int bearing = (int) sample[0];
-			System.out.println("Bearing: " + bearing);
 			int distance = (int) sample[1];
-			System.out.println("Distance: " + distance);
-
+			System.out.println("1. Bearing/Distance: " + bearing + " " + distance);
 			while (bearing != 0 && distance != 0) {
-
+				// while (bearing != 0 && distance != 0) will never be the condition to roam.
+				// Roaming should happen
+				// while (bearing = 0 && distance != 0) OR while (distance > 100000)
 				if (distance > MAXIMUM_RANGE_IR_SENSOR) {
-					// dit werkt niet omdat roam alleen begint als bearing !=0
-					// echter if (niet-in-range) dan geldt bearing ==0;
-					// dus roam start nooit omdat bearing altijd 0 is.
 
-					System.out.println("in de tweede while loop");
+					System.out.println("2. Roam");
 					drive.roam(beaconFound); // begin met roam
-					System.out.println("Start roaming");
+					System.out.println("2. R | Bearing/Distance: " + bearing + " " + distance);
 				} else if (distance <= MAXIMUM_RANGE_IR_SENSOR) { // maximum range IR sensor
 					setBeaconFound(true);
-					System.out.println("Beacon found, going to beacon");
+					System.out.println("2. IR | before turn bearing: " + bearing);
 					drive.turn(bearing); // gaat de robot in de richting van het beacon roteren met "bearing" graden
 					seek.fetchSample(sample, 0);
 					bearing = (int) sample[0];
+					System.out.println("2. IR | after turn drive distance: " + distance);
 					drive.straight(distance); // gaat de robot rechtuit rijden gedurende afstand "distance"
 					seek.fetchSample(sample, 0);
 					distance = (int) sample[1];
-			}
-
+					System.out.println("2. IR | after drive bearing: " + bearing);
+					System.out.println("2. IR | after drive distance: " + distance);
+				}
 			}
 		}
-	//	ir.close();
+		// ir.close();
 	}
 }
-
