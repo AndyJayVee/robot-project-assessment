c65d0a22486d91a5645df3d7fd1ca052bc916de0
L. van Vark <loek.van.vark@hva.nl>
2019-01-14 15:02:04 +0100

    Replace BeaconFinderLoek.java with loekbranch version. Will probably not work yet

--- C14-F/src/models/BeaconFinderLoek.java
+++ C14-F/src/models/BeaconFinderLoek.java
@@ -1,9 +1,7 @@
-package models;
+// BeaconFinder met verbeteringen
+// zonder methods
 
-/**
- * @author loek
- * Adjustment: improve start/roam functionality && improve readbility in logic
- */
+package models;
 
 import lejos.hardware.Button;
 import lejos.hardware.port.SensorPort;
@@ -14,19 +12,20 @@ import models.Pilot;
 
 public class BeaconFinderLoek {
 
-	private static final int MAXIMUM_RANGE_IR_SENSOR = 150;
+	private static final int MAXIMUM_RANGE_IR_SENSOR = 150; // de maximum range is tussen de 100~200 centimeter
+	// afhankelijk van de bron
 
-	private EV3IRSensor ir = new EV3IRSensor(SensorPort.S4);
-	SensorMode seek = ir.getSeekMode(); // TODO small explanation seekmode?
+	EV3IRSensor ir = new EV3IRSensor(SensorPort.S4); // activeert een nieuwe IR-sensor op poort S4
+	SensorMode seek = ir.getSeekMode(); // activeert de Seek modus
+	float[] sample = new float[seek.sampleSize()]; // maakt array met sample informatie
+	// edit Loek
+	// initialize the Pilot and Driving in a correct manner
 	Driving drive = new Driving();
 	Pilot pilot = new Pilot();
-
-	float[] sample = new float[seek.sampleSize()];
-
+	private boolean beaconFound = false;
+	
 	private int bearing;
 	private int distance;
-
-	private boolean beaconFound = false;
 
 	public boolean isBeaconFound() {
 		return beaconFound;
@@ -40,71 +39,53 @@ public class BeaconFinderLoek {
 		super();
 	}
 
-	/**
-	 * if invoked will start measuring + moving. Can be invoked from random position
-	 * Tests whether in range (moves to beacon or not in range (roam)
-	 */
+	// vanaf hier is de IR-sensorcode:
+
 	public void findBeacon() {
-		// TODO not sure why exitkey is needed?
+		// @aut Loek: edit: instantiate Pilot and Driving
+		Pilot pilot = new Pilot();
+		Driving drive = new Driving(pilot.getPilot());
+
 		while (Button.ESCAPE.isUp()) {
-			// Fetch an initial measurement for distance and store this value
-			distance = fetchDistance();
-			System.out.println("1st while distance: " + distance);
-
-			// if not in Range, fetch distance will give infinite
-			// so: while (distance is inifite keep roaming), after distance becomes lower,
-			// break out while, and start inRange()
-			while (distance > 150) {
-				// start roam
-				drive.straight(100);
-				// during roam keep fetching and updating local distance variable
-				// this to break out of this while()
-				distance = fetchDistance();
-				System.out.println("Roam | Distance: " + distance);
-
-			}
-			// TODO do we need a driving.stopRoam to prevent eternal roaming??
+			seek.fetchSample(sample, 0);
 			
-			// now status == beaconFound(true) as we're in range of sensor
-			// while not at beacon yet keep doing inRange()
-			while (distance > 0) {
-				inRange();
-				
-				// If Marvin has arrived at beacon, it should break from this while
-				// following line will ensure that it breaks from current while loop
-				// at least when it measures distance smaller than the 0 
-				distance = fetchDistance();
-//				System.out.println("Found | Distance: " + distance);
+			//System.out.println("1st while. Bearing: " + bearing);
+			distance = (int) sample[1];
+			System.out.println("1st while. Distance: " + distance);
+			while (distance < 3) { // IRSensor is not perfect, so assuming distance < 3 means Marvin reached beacon
+				if (distance > 21474836) {
+					setBeaconFound(false);
+					seek.fetchSample(sample, 0);
+					bearing = (int) sample[0];
+					distance = (int) sample[1];
+					// drive.roam(beaconFound); // start roam
+					System.out.println("2. Roam | Distance: " + distance);
+					System.out.println("2. Roam | Bearing: " + bearing);
+					drive.straight(30);
+					seek.fetchSample(sample, 0);
+					distance = (int) sample[1];
+				} else { // inRange --> turn and drive to beacon
+					setBeaconFound(true);
+					seek.fetchSample(sample, 0);
+					bearing = (int) sample[0];
+					distance = (int) sample[1];
+					System.out.println("2. inRange | Distance: " + distance);
+					System.out.println("2. inRange | Bearing: " + bearing);
+					// turn with bearing
+					drive.turn(bearing); 
+					seek.fetchSample(sample, 0);
+					bearing = (int) sample[0];
+					// drive distance to beacon
+					drive.straight(distance);
+					// fetch another sample to test if movement was sufficient
+					seek.fetchSample(sample, 0);
+					distance = (int) sample[1];
+				}
 			}
-			// TODO do we need a command to let Marvin stop? 
 		}
-			// TODO or needs te stop command come here, when Excape key is pressed?
+		// ir.close();
 	}
-	/**
-	 * Puts robot in mode to turn towards beacon and drive towards it
-	 */
-	private void inRange() {
-		// this will let other classes read the latest status of whether
-		// distance/bearing are known or not
-		// by now bearing and distance are measured == known == found(true)
-		setBeaconFound(true);
-
-		System.out.println("inRange() | Bearing: " + bearing);
-		System.out.println("inRange() | Distance: " + distance);
-		
-		bearing = fetchBearing();
-		drive.turn(bearing);
-
-		// do another fetch (in a perfect world bearing would be 0 after latest
-		// fetch&turn)
-		bearing = fetchBearing();		
-		// drive the distance as fetched
-		drive.straight(distance);
-		// fetch again and adjust bearing/distance if needed
-		bearing = fetchBearing();
-		distance = fetchDistance();
-	}
-
+	
 	/**
 	 * @return latest fetch of bearing measurement from Sensor (int)
 	 */
@@ -124,15 +105,3 @@ public class BeaconFinderLoek {
 		return distance;
 	}
 }
-
-/** After update redundant, keeping it for now
- * Sets robot in roam mode
- */
-
-//public void roamMode() {
-//	drive.roam(beaconFound); // start roam
-//
-//	// fetch measurement and store the value
-//	seek.fetchSample(sample, 0);
-//	distance = (int) sample[1];
-//}
