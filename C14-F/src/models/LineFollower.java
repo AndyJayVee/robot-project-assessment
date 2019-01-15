a1c8e22f557f85b2286f10d2428f281bbeda11f6
Frank Dennissen <frank.dennissen@hva.nl>
2019-01-15 19:42:31 +0100

    Update on linefollower and pilot

--- C14-F/src/models/LineFollower.java
+++ C14-F/src/models/LineFollower.java
@@ -3,24 +3,12 @@ package models;
 import lejos.hardware.Button;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
-<<<<<<< HEAD
-import models.Pilot;
-
-public class LineFollower {
-
-	private static EV3ColorSensor sensor = new EV3ColorSensor(SensorPort.S2);
-	private Stopwatch stopwatch = new Stopwatch(true);
-	private Thread stopwatchThread = new Thread(stopwatch);
-	private float[] scannedColor = new float[1];
-	private Pilot pilot = new Pilot();
-=======
 import lejos.utility.Delay;
 
 public class LineFollower {
 
 	private EV3ColorSensor sensor = new EV3ColorSensor(SensorPort.S2);
 	private MarvinMover marvinMover = new MarvinMover();
->>>>>>> 9e2e9884232eca8f141f9f6f355e6e35279a0df8
 
 	public LineFollower() {
 		super();
@@ -31,29 +19,6 @@ public class LineFollower {
 	 * border of the black line / with the line on the left of marvin
 	 */
 
-<<<<<<< HEAD
-	public void followLine() {
-		pilot.setAngularSpeed(90000);
-		pilot.setLinearSpeed(150);
-		System.out.println("A");
-		stopwatchThread.start();
-		System.out.println("B");
-		sensor.setCurrentMode("Red");
-		// while not pressed continue
-		while (Button.ESCAPE.isUp()) {
-			while (Button.ENTER.isUp()) {
-				sensor.fetchSample(scannedColor, 0);
-				if (scannedColor[0] > .60) { // white
-					System.out.println("C");
-					pilot.rotateRight();
-				} else if (scannedColor[0] < .20) { // black
-					pilot.rotateLeft();
-					System.out.println("D");
-				} else { // grey
-					pilot.backward();
-					System.out.println("E");
-				}
-=======
 		public void followLine() {
 		System.out.println("followLine started");
 		// initialize array to fetch sample in
@@ -70,11 +35,8 @@ public class LineFollower {
 				marvinMover.turnRightOnBlack();
 			} else { // grey
 				marvinMover.driveStraightOnGrey();
->>>>>>> 9e2e9884232eca8f141f9f6f355e6e35279a0df8
 			}
-			stopwatch.setNotStopped(false);
 		}
 		stopwatch.setNotStopped(false);
-		Delay.msDelay(10000);
 	}
 }
--- C14-F/src/models/Pilot.java
+++ C14-F/src/models/Pilot.java
@@ -6,25 +6,47 @@ import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
 import lejos.robotics.navigation.MovePilot;
 
-public class Pilot extends MovePilot{
+public class Pilot {
 
-	private static final double WHEEL_DIAMETER = 43.2; // Wheel diameter and offset (half track width) is set in mm.
-	private static final double TRACK_WIDTH = 114.0;
-		
+	MovePilot pilot;
+	Driving drive;
+	private static final double MAX_LINEAR_SPEED = 350;
+//	private static final double MAX_ANGULAR_SPEED = 0;
 	
-	public Pilot(Chassis chassis) {
-		super(chassis);
+	//Wheel diameter and offset (half track with) is set in mm.
+	private static final double WHEEL_DIAMETER = 43.2;
+	private static final double TRACK_WIDTH = 114.0;
+	private double linearSpeed = MAX_LINEAR_SPEED;
+	private double angularSpeed = 350;
+	
+	public void setLinearSpeed(double linearSpeed) {
+		this.linearSpeed = linearSpeed;
 	}
 
-	public Pilot() {	
-		super(new WheeledChassis(
-				new Wheel[] {WheeledChassis.modelWheel(Motor.A, WHEEL_DIAMETER).offset(-0.5 * TRACK_WIDTH), 
-							WheeledChassis.modelWheel(Motor.D, WHEEL_DIAMETER).offset(0.5 * TRACK_WIDTH)}, 
-				WheeledChassis.TYPE_DIFFERENTIAL));
-	}	
+	public void setAngularSpeed(double angularSpeed) {
+		this.angularSpeed = angularSpeed;
+	}
+	
+	public MovePilot getPilot() {
+		return pilot;
+	}
+	
+	//Pilot constructor
+	public Pilot() {
+	Wheel leftWheel = WheeledChassis.modelWheel(Motor.A, WHEEL_DIAMETER).offset(-0.5 * TRACK_WIDTH);
+	Wheel rightWheel = WheeledChassis.modelWheel(Motor.D, WHEEL_DIAMETER).offset(0.5 * TRACK_WIDTH);
+	Chassis chassis = new WheeledChassis(new Wheel[] {leftWheel, rightWheel}, WheeledChassis.TYPE_DIFFERENTIAL);
+	pilot = new MovePilot(chassis);
+	
+	drive = new Driving(pilot);
+	drive.travelAndRotate();
+	 
+		pilot.setLinearSpeed(linearSpeed);
+		pilot.setAngularSpeed(angularSpeed);
+	
+	pilot.stop();
+	}
 }
-	
-	
     
     
 
