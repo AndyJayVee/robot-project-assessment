c863642dcbf953f83eb2a39123ee4593f3113c6c
L. van Vark <loek.van.vark@hva.nl>
2019-01-15 11:53:06 +0100

    Replace LineFollowerLoek.java invokes MarvinMover

--- C14-F/src/models/LineFollowerLoek.java
+++ C14-F/src/models/LineFollowerLoek.java
@@ -6,25 +6,30 @@ import lejos.hardware.Button;
 import lejos.hardware.Key;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import models.Driving;
 import models.Pilot;
-
+import models.MarvinMover;
 
 public class LineFollowerLoek { //implements Runnable {
 
 	static Brick brick;
 	static EV3ColorSensor sensor = new EV3ColorSensor(SensorPort.S2);
 
-	static UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
-    static UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.D);
+	static EV3LargeRegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);
+    static EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.D);
+    
+    static MarvinMover marvinMover;
     
 	public LineFollowerLoek() {
 		super();
 	}
+	
+	LineFollowerLoek lineFollowerLoek = new LineFollowerLoek();
 
 	/**
 	 * method to follow a line / this works best if the marvin is placed on the
@@ -39,14 +44,11 @@ public class LineFollowerLoek { //implements Runnable {
 		while (Button.DOWN.isUp()) {
 			sensor.fetchSample(scannedColor, 0);
 			if (scannedColor[0] > .60) { // white
-				motorA.setPower(-4);
-                motorB.setPower(-44);
+				marvinMover.turnLeftOnWhite();
 			} else if (scannedColor[0] < .20) { // black
-				motorA.setPower(-48);
-                motorB.setPower(0);
+				marvinMover.turnRightOnBlack();
 			} else { // grey
-		        motorA.setPower(-45);
-		        motorB.setPower(-45);
+		        marvinMover.driveStraightOnGrey();
 			}
 		}
 	}
