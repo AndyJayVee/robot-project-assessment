50e7deb820c9784a2a449c365f6deb62efb5e327
Loek van Vark <loek.van.vark@hva.nl>
2019-01-15 17:58:33 +0100

    tweaks in ID

--- C14-F/src/models/LineFollowerID.java
+++ C14-F/src/models/LineFollowerID.java
@@ -5,12 +5,18 @@ import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.utility.Delay;
 
-public class LineFollowerRGB {
+public class LineFollowerID {
 
 	private EV3ColorSensor sensor = new EV3ColorSensor(SensorPort.S2);
 	private MarvinMover marvinMover = new MarvinMover();
+	
+	// calibrated/tested with ColorSensor ID mode
+	private final static int black = 7;
+//  private final static int grey;
+	private final static int red = 0;
+	private final static int white = 6;
 
-	public LineFollowerRGB() {
+	public LineFollowerID() {
 		super();
 	}
 
@@ -23,32 +29,20 @@ public class LineFollowerRGB {
 		System.out.println("followLine started");
 		// initialize array to fetch sample in
 		float[] scannedColor = new float[3];
-
-		// calculate avg of samples
-		float total = 0;
-		float avgSamples;
-		for (int i = 0; i < scannedColor.length; i++) {
-			total = total + scannedColor[i];
-		}
-		avgSamples = total / scannedColor.length;
-
 		Stopwatch stopwatch = new Stopwatch(true);
 		Thread stopwatchThread = new Thread(stopwatch);
 		stopwatchThread.start();
-		// set sensor to 3 colors mode
+		// set sensor to 3 colors mode, gives int as color
 		sensor.setCurrentMode("ColorID");
-		while (Button.DOWN.isUp()) {
-			int currentColor = sensor.getColorID();
-			int black = 7;
-//			int grey;
-			int red = 0;
-			int white = 6;
-			int lapCount = 0;
+		int currentColor = sensor.getColorID();
 
-			if  (currentColor == red && lapCount == 0) {
-				marvinMover.cruise();
+		while (Button.DOWN.isUp()) {
+	
+			int lapCount = 0; // initialize on 0
+			if (currentColor == red && lapCount == 0) {
+				marvinMover.driveStraightOnGrey();
 //				start.timer
-//					lapCount++; 
+//					lapCount++;
 //				 } else if (currentColor == red && lapCount > 0) {
 //				 	stop.timer();
 //				 	lapTime[lapCount] = getLapTime();
@@ -56,32 +50,21 @@ public class LineFollowerRGB {
 //					lapCount++;
 //				 }
 //				 	print ("Lap "lapCount + " " + lapTime[lapCount - 1]) ??
-			} else if (currentColor == white) { 
+			} else if (currentColor == red && lapCount != 0) {
+//				 stop.timer();
+//				 lapTime[lapCount] = getLapTime();
+//				 reset.timer();
+//				 lapCount++;
+//				 start.timer();
+			} else if (currentColor == white) {
 				marvinMover.turnLeftOnWhite();
-			} else if (currentColor == black)  { // black
+			} else if (currentColor == black) { // black
 				marvinMover.turnRightOnBlack();
-			} else {
-				marvinMover.driveStraightOnGrey();// grey
-			}
-			{ // grey
+			} else { // all other values are considered grey
 				marvinMover.driveStraightOnGrey();
 			}
-			// stopwatch.setNotStopped(false);
-			// Delay.msDelay(10000);
+//			 stopwatch.setNotStopped(false);
+//			 Delay.msDelay(10000);
 		}
 	}
-	
-	
-	// TODO
-				// if (sensor measures red finish line && lapCount == 0) {
-				//	start.timer
-				//	lapCount++; 
-				// } else if (sensor measures red finish line && lapCount > 0) {
-				// 	stop.timer();
-				// 	lapTime[lapCount] = getLapTime();
-				// 	reset.timer();
-				//	lapCount++;
-				// }
-				// 	print ("Lap "lapCount + " " + lapTime[lapCount - 1]) ??
-
 }
