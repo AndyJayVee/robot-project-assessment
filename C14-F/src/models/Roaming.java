18aefeebb815820867080a943c515b9f332459a9
Frank Dennissen <frank.dennissen@hva.nl>
2019-01-15 19:52:51 +0100

    Class roaming (extends driving, implements runnable) has been added to the project. This should enable to thread in Beaconfinder.

---
+++ C14-F/src/models/Roaming.java
@@ -0,0 +1,30 @@
+package models;
+
+public class Roaming extends Driving implements Runnable{
+	
+	private boolean BeaconFound;
+	
+	public Roaming(boolean beaconFound) {
+		super();
+		BeaconFound = beaconFound;
+	}
+
+	public void setBeaconFound(boolean beaconFound) {
+		BeaconFound = beaconFound;
+	}
+
+	public void run() {
+		try {
+			while (!BeaconFound) {
+				for (int i = 0; i < 11; i++) {
+					super.pilot.travel(500);
+					super.pilot.rotate(160);
+				}
+			}
+				
+		} catch (Exception e) {
+			System.out.printf("Oops, something went\n wrong with roaming");
+		}
+	}
+
+}
