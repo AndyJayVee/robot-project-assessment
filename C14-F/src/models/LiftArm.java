a047377fba6a49660cac4d84118568972e8e4072
L.D.B. Snel <leo.snel@hva.nl>
2019-01-15 15:22:29 +0100

    Update LiftArm.java

--- C14-F/src/models/LiftArm.java
+++ C14-F/src/models/LiftArm.java
@@ -7,9 +7,9 @@ public class LiftArm {
 
 	private EV3LargeRegulatedMotor liftArm = new EV3LargeRegulatedMotor(MotorPort.B);// instantiate the connection of
 																						// the liftArm to port B
-	private EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);// instantiate the left motor
-
-	private EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);// instantiate the right motor
+//	private EV3LargeRegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);// instantiate the left motor
+//
+//	private EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.D);// instantiate the right motor
 
 	// no args constructor
 	public LiftArm() {
@@ -20,20 +20,14 @@ public class LiftArm {
 
 	public void liftUp() { // rotates the motor at a set speed and number of rotations
 		liftArm.setSpeed(25); // initial speed set @25
-		liftArm.rotate(90); // initial degrees of rotations set @90
-	}
-
-	public void driveWithBeacon() { // moves vehicle
-		for (int i = 0; i < 5; i++) { // drive a left-hand circle (left motor rotates twice as much and twice as fast as right motor)
-			leftMotor.setSpeed(200);
-			leftMotor.rotate(220);
-			rightMotor.setSpeed(100);
-			rightMotor.rotate(30);
-		}
+		liftArm.rotate(120); // initial degrees of rotations set @90
 	}
 
 	public void liftDown() { // rotates the motor at a set speed and number of rotations
-		liftArm.setSpeed(25); // initial speed set @25
-		liftArm.rotate(-90); // initial degrees of rotations set @-90
+		liftArm.setSpeed(15); // initial speed set @15
+		liftArm.rotate(-120); // initial degrees of rotations set @-70
 	}
+	
+	// methods for moving the vehicle were moved to MarvinMover.java
+
 }
