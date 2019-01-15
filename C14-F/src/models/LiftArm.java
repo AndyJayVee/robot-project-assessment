ed2a915eb505b54c0a760912119cccf6231a72ca
Leo Snel <leo.snel@hva.nl>
2019-01-15 11:58:45 +0100

    major changes to LiftArm and MoveBeacon

--- C14-F/src/models/LiftArm.java
+++ C14-F/src/models/LiftArm.java
@@ -2,6 +2,7 @@ package models;
 
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
+import lejos.utility.Delay;
 
 public class LiftArm {
 
@@ -23,17 +24,44 @@ public class LiftArm {
 		liftArm.rotate(90); // initial degrees of rotations set @90
 	}
 
-	public void driveWithBeacon() { // moves vehicle
-		for (int i = 0; i < 5; i++) { // drive a left-hand circle (left motor rotates twice as much and twice as fast as right motor)
-			leftMotor.setSpeed(200);
-			leftMotor.rotate(220);
-			rightMotor.setSpeed(100);
-			rightMotor.rotate(30);
-		}
-	}
-
 	public void liftDown() { // rotates the motor at a set speed and number of rotations
 		liftArm.setSpeed(25); // initial speed set @25
 		liftArm.rotate(-90); // initial degrees of rotations set @-90
 	}
+	
+	// methods for moving the vehicle
+
+	public void turn90DegreesLeft() { // turns vehicle
+		leftMotor.setSpeed(200); // 90 degrees left
+		leftMotor.rotate(500);
+	}
+
+	public void turn90DegreesRight() { // turns vehicle
+		rightMotor.setSpeed(200); // 90 degrees right
+		rightMotor.rotate(500);
+	}
+
+	public void turn45DegreesLeft() { // turns vehicle
+		leftMotor.setSpeed(200); // 45 degrees left
+		leftMotor.rotate(250);
+	}
+	
+	public void turn45DegreesRight() { // turns vehicle
+		rightMotor.setSpeed(200); // 45 degrees right
+		rightMotor.rotate(250);
+	}
+
+	public void turn180Degrees() { // turns vehicle
+		rightMotor.setSpeed(200); // 180 degrees right
+		rightMotor.rotate(1000);
+	}
+
+	public void driveForward() { // drives vehicle forward
+		rightMotor.setSpeed(200);
+		rightMotor.forward();
+		leftMotor.setSpeed(200);
+		leftMotor.forward();
+		Delay.msDelay(8000);
+	}
+
 }
--- C14-F/src/models/MoveBeacon.java
+++ C14-F/src/models/MoveBeacon.java
@@ -12,16 +12,46 @@ public class MoveBeacon {
 		super();
 	}
 
-	// method to grab the beacon and put it down at a programmed location via LiftArm.java
+	// method to grab the beacon, move it in an F-shape, and put it down
 	public void grabBeacon() {
-        Button.LEDPattern(4);    // flash green led and 
-        Sound.beepSequenceUp();  // make sound when ready.
+//        Button.LEDPattern(4);    // flash green led and 
+//        Sound.beepSequenceUp();  // make sound when ready.
 		arm.liftUp(); // lifts beacon up
 		System.out.println("lifted up");
-		arm.driveWithBeacon(); // moves beacon (according to LiftArm.java specifications)
-		System.out.println("moved beacon to other location");
+
+		arm.driveForward(); // leg 1a. moves beacon (according to LiftArm.java specifications)
+		System.out.println("drove forward");
+
+		arm.driveForward(); // leg 1b. moves beacon (according to LiftArm.java specifications)
+		System.out.println("drove forward");
+
+		arm.turn90DegreesRight(); // leg 2. moves beacon (according to LiftArm.java specifications)
+		System.out.println("turned right");
+
+		arm.driveForward(); // leg 2. moves beacon (according to LiftArm.java specifications)
+		System.out.println("drove forward");
+
+		arm.turn180Degrees(); // leg 3. moves beacon (according to LiftArm.java specifications)
+		System.out.println("turned 180 degrees");
+
+		arm.driveForward(); // leg 3. moves beacon (according to LiftArm.java specifications)
+		System.out.println("drove forward");
+
+		arm.turn90DegreesLeft(); // leg 4. moves beacon (according to LiftArm.java specifications)
+		System.out.println("turned left");
+
+		arm.driveForward(); // leg 4. moves beacon (according to LiftArm.java specifications)
+		System.out.println("drove forward");
+
+		arm.turn90DegreesLeft(); // leg 5. moves beacon (according to LiftArm.java specifications)
+		System.out.println("turned left");
+
+		arm.driveForward(); // leg 5. moves beacon (according to LiftArm.java specifications)
+		System.out.println("drove forward");
+
 		arm.liftDown(); // puts beacon down
 		System.out.println("put down");
-		Sound.beepSequence(); // finished grabBeacon method.
+
+//		Sound.beepSequence(); // finished grabBeacon method.
 	}
 }
