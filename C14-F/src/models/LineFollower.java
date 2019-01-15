0f116afc5373bcf87ae4dade98afdaf8c402f5f4
L. van Vark <loek.van.vark@hva.nl>
2019-01-15 14:21:14 +0100

    After test TUE 1415. Needs tweaking, works on MarvinMover.

--- C14-F/src/models/LineFollower.java
+++ C14-F/src/models/LineFollower.java
@@ -1,21 +1,15 @@
 package models;
 
-import lejos.hardware.Brick;
 import lejos.hardware.Button;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.utility.Delay;
-import models.MarvinMover;
 
-public class LineFollower {
+public class LineFollower { //implements Runnable {
 
-	static Brick brick;
-	private static EV3ColorSensor sensor = new EV3ColorSensor(SensorPort.S2);
+//	static Brick brick;
+	private EV3ColorSensor sensor = new EV3ColorSensor(SensorPort.S2);
 	private MarvinMover marvinMover = new MarvinMover();
-	private Stopwatch stopwatch = new Stopwatch(true);
-	private Thread stopwatchThread = new Thread(stopwatch);
-	private float[] scannedColor = new float[1];
-	
+
 	public LineFollower() {
 		super();
 	}
@@ -24,20 +18,25 @@ public class LineFollower {
 	 * method to follow a line / this works best if the marvin is placed on the
 	 * border of the black line / with the line on the left of marvin
 	 */
+
 		public void followLine() {
+		System.out.println("followLine started");
+		// initialize array to fetch sample in
+		float[] scannedColor = new float[1];
 		sensor.setCurrentMode("Red");
-		stopwatchThread.start();
+
 		while (Button.DOWN.isUp()) {
 			sensor.fetchSample(scannedColor, 0);
 			if (scannedColor[0] > .60) { // white
+				System.out.println("White");
 				marvinMover.turnLeftOnWhite();
 			} else if (scannedColor[0] < .20) { // black
+				System.out.println("Black");
 				marvinMover.turnRightOnBlack();
 			} else { // grey
-		        marvinMover.driveStraightOnGrey();
+				System.out.println("Grey");
+				marvinMover.driveStraightOnGrey();
 			}
 		}
-		stopwatch.setNotStopped(false);
-		Delay.msDelay(10000);
 	}
 }
