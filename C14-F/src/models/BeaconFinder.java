8632bbe839bc9a6dc9c77c62f5018482f96c1f06
L. van Vark <loek.van.vark@hva.nl>
2019-01-16 14:17:00 +0100

    Update BeaconFinder.java with WED TEST LOEK - METHODS and clean

--- C14-F/src/models/BeaconFinder.java
+++ C14-F/src/models/BeaconFinder.java
@@ -1,38 +1,26 @@
-/**
- * MONDAY Working version, checks if needs to roam or turn/drive	
- */
 package models;
 
 import lejos.hardware.Button;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.SensorMode;
-import models.Driving;
 import models.Pilot;
-import models.MarvinMover;
 
 public class BeaconFinder {
 
-	private static final int ROAM_DISTANCE = 21474836;
-
-	private static final int MAXIMUM_RANGE_IR_SENSOR = 150; // based on max range Sensor
+	private static final int DISTANCE_TRESHOLD_ROAM = 21474836;
 
 	private EV3IRSensor ir = new EV3IRSensor(SensorPort.S4); // use port S4 for IR Sensor
-	private SensorMode seek = ir.getSeekMode(); // initiate seekmode
-	private MarvinMover marvinMover = new MarvinMover();
-
+	private SensorMode seek = ir.getSeekMode(); // initiate seekmode in Sensor
 	private float[] sample = new float[seek.sampleSize()]; // declare array to store samples form Sensor
-	private boolean beaconFound = false;
-
+	private boolean beaconFound;
 	private int bearing;
 	private int distance;
 
+	private static Pilot pilot = new Pilot();
+
 	public boolean isBeaconFound() {
 		return beaconFound;
-	}
-
-	public void setBeaconFound(boolean beaconFound) {
-		this.beaconFound = beaconFound;
 	}
 
 	// no args cons
@@ -41,50 +29,65 @@ public class BeaconFinder {
 	}
 
 	/**
-	 * If placed within range, will turn and drive towards
+	 * When placed in area with beacon, it will roam until sensor will measure some
+	 * values When inRange it should switch to: turn&drive towards beacon. NOTE:
+	 * sensor is slow. Works, but it's not pretty
 	 */
 	public void findBeacon() {
-		Pilot pilot = new Pilot();
-		Driving drive = new Driving(pilot.getPilot());
-		MarvinMover marvinMover = new MarvinMover();
-
-
 		while (Button.DOWN.isUp()) {
-			// TODO seek.fetchSample(sample, 0); 
-			
-			//System.out.println("1st while. Bearing: " + bearing);
 			distance = fetchDistance();
 			System.out.println("1st while. Distance: " + distance);
+			Roaming roaming = new Roaming(beaconFound);
+			Thread roamThread = new Thread(roaming);
 			while (distance > 0) {
-				while (distance >= ROAM_DISTANCE) {					
-					bearing = fetchBearing();
-					distance = fetchDistance();
-					System.out.println("2. Roaming | Distance: " + distance);
-					System.out.println("2. Roaming | Bearing: " + bearing);
-					marvinMover.roam(beaconFound);
-					distance = fetchDistance();
+				if (distance >= DISTANCE_TRESHOLD_ROAM) {
+					roamThread.start();
 				}
-				while (distance < ROAM_DISTANCE) { // inRange --> turn and drive to beacon
-					setBeaconFound(true);
-					bearing = fetchBearing();	
-					distance = fetchDistance();
-					System.out.println("2. inRange | Distance: " + distance);
-					System.out.println("2. inRange | Bearing: " + bearing);
-					// turn with bearing
-					drive.turn(bearing); 	
-					distance = fetchDistance();	
-					// drive distance to beacon
-					drive.straight(distance);
-					// fetch another sample to test if movement was sufficient
-					distance = fetchDistance();
-					bearing = fetchBearing();
+				while (distance >= DISTANCE_TRESHOLD_ROAM) {
+					seekRange();
+				}
+				roaming.setStopRoaming(true);
+				while (distance < DISTANCE_TRESHOLD_ROAM) { // inRange --> turn and drive to beacon
+					inRange();
+
 				}
 			}
 		}
 		ir.close();
 	}
+/**
+ * initiates turn&drive towards beacon
+ */
+	private void inRange() {
+		beaconFound = true;
+		bearing = fetchBearing();
+		distance = fetchDistance();
+		System.out.println("2. inRange | Distance: " + distance);
+		System.out.println("2. inRange | Bearing: " + bearing);
+		// turn with bearing, use Pilot
+		pilot.rotate(-bearing);
+		distance = fetchDistance();
+		// drive distance to beacon
+		pilot.travel(distance * 10);
+		// fetch another sample to test if movement was sufficient
+		distance = fetchDistance();
+		bearing = fetchBearing();
+	}
+
+	/**
+	 * initiates roamingMode from Pilot
+	 */
+	private void seekRange() {
+		beaconFound = false;
+		bearing = fetchBearing();
+		distance = fetchDistance();
+		System.out.println("2. Roaming | Distance: " + distance);
+		System.out.println("2. Roaming | Bearing: " + bearing);
+		distance = fetchDistance();
+	}
+
 	/*
-	 * @return Fetch of bearing measurement from Sensor
+	 * @return Fetch bearing measurement from Sensor
 	 */
 	public int fetchBearing() {
 		// fetch measurement and store the value
@@ -92,8 +95,9 @@ public class BeaconFinder {
 		bearing = (int) sample[0];
 		return bearing;
 	}
+
 	/**
-	 * @return Fetch of distance measurement from Senso
+	 * @return Fetch distance measurement from Sensor
 	 */
 	public int fetchDistance() {
 		// fetch measurement and store the value
