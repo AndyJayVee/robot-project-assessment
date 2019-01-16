3fca9760430bf2a3b774490a457efbcdde298c79
Frank Dennissen <frank.dennissen@hva.nl>
2019-01-16 15:57:08 +0100

    Working version of LineFollower committed.

--- C14-F/src/models/LineFollower.java
+++ C14-F/src/models/LineFollower.java
@@ -31,9 +31,6 @@ public class LineFollower {
 		sensor.setCurrentMode("Red");
 		while (Button.DOWN.isUp()) {
 			sensor.fetchSample(scannedColor, 0);
-			if (stopRacing()) {
-				break;
-			}
 			if (scannedColor[0] > .60) { // white
 				marvinMover.turnLeftOnWhite();
 			} else if (scannedColor[0] < .20) { // black
@@ -45,17 +42,17 @@ public class LineFollower {
 		stopwatch.setNotStopped(false);
 		Delay.msDelay(10000);
 	}
-		public boolean stopRacing() {
-			boolean stop = false;
-			EV3IRSensor ir = new EV3IRSensor(SensorPort.S4);
-			SensorMode seek = ir.getSeekMode();
-			float[] sample = new float[seek.sampleSize()];
-			seek.fetchSample(sample, 0);
-			int distance = (int) sample[1];
-			ir.close();
-			if (distance <1000) {
-				stop = true;
-			}
-			return stop;
-		}	
+//		public boolean stopRacing() {
+//			boolean stop = false;
+//			EV3IRSensor ir = new EV3IRSensor(SensorPort.S4);
+//			SensorMode seek = ir.getSeekMode();
+//			float[] sample = new float[seek.sampleSize()];
+//			seek.fetchSample(sample, 0);
+//			int distance = (int) sample[1];
+//			ir.close();
+//			if (distance <1000) {
+//				stop = true;
+//			}
+//			return stop;
+//		}	
 }
