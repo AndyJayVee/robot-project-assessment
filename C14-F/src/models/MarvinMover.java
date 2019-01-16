d2cc83255286ae3ad6f5f73efb51be118032085a
L.D.B. Snel <leo.snel@hva.nl>
2019-01-16 10:22:07 +0100

    updated methods turn180DegreesClockwise(), turn180DegreesCounterClockwise(), turn90DegreesClockwise(),turn90DegreesCounterClockwise()

--- C14-F/src/models/MarvinMover.java
+++ C14-F/src/models/MarvinMover.java
@@ -1,5 +1,5 @@
 /**
- * Has methods to set motor speeds to move Marvin
+ * Has methods to set motorspeeds to move Marvin
  */
 
 package models;
@@ -8,7 +8,7 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
 
-public class MarvinMover extends Driving {
+public class MarvinMover {
 
 	static EV3LargeRegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);
 	static EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.D);
@@ -38,7 +38,7 @@ public class MarvinMover extends Driving {
 		motorB.backward();
 	}
 
-	public void turn90DegreesRight() { // turns vehicle 90 degrees right
+	public void turn180DegreesCounterClockwise() { // turns vehicle 90 degrees clockwise
 		motorB.setSpeed(80);
 		motorA.setSpeed(-300);
 		motorB.forward();
@@ -46,7 +46,7 @@ public class MarvinMover extends Driving {
 		Delay.msDelay(1400);
 	}
 
-	public void turn90DegreesLeft() { // turns vehicle 90 degrees left
+	public void turn90DegreesCounterClockwise() { // turns vehicle 90 degrees counter clockwise
 		motorA.setSpeed(80);
 		motorB.setSpeed(-300);
 		motorA.forward();
@@ -54,15 +54,23 @@ public class MarvinMover extends Driving {
 		Delay.msDelay(1400);
 	}
 
-	public void turn180Degrees() { // turns vehicle 180 degrees left hand side
+	public void turn180DegreesCounterClockwise() { // turns vehicle 180 degrees counter clockwise
 		motorA.setSpeed(80);
 		motorB.setSpeed(-300);
 		motorA.forward();
 		motorB.backward();
 		Delay.msDelay(2600);
 	}
+	
+	public void turn180DegreesClockwise() { // turns vehicle 180 degrees clockwise
+		motorB.setSpeed(80);
+		motorA.setSpeed(-300);
+		motorB.forward();
+		motorA.backward();
+		Delay.msDelay(2600);
+	}
 
-	public void shortLeg() { // drives vehicle forward
+	public void shortLeg() { // drives vehicle forward by approximately 20 cm
 		motorA.setSpeed(400);
 		motorA.forward();
 		motorB.setSpeed(400);
@@ -70,7 +78,7 @@ public class MarvinMover extends Driving {
 		Delay.msDelay(1400);
 	}
 
-	public void longLeg() { // drives vehicle forward
+	public void longLeg() { // drives vehicle forward by approximately 40 cm
 		motorA.setSpeed(400);
 		motorA.forward();
 		motorB.setSpeed(400);
