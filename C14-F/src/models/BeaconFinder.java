f11828ed4f67a91113b2693da96087cce01bf93e
Frank Dennissen <frank.dennissen@hva.nl>
2019-01-17 15:04:43 +0100

    Alles wat niet werk uit beaconfinder weggecomment

--- C14-F/src/models/BeaconFinder.java
+++ C14-F/src/models/BeaconFinder.java
@@ -6,7 +6,6 @@
 
 package models;
 
-import lejos.hardware.Button;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.SensorMode;
@@ -15,16 +14,16 @@ import models.Roamer;
 
 public class BeaconFinder {
 
-	private static final int DISTANCE_TRESHOLD_ROAM = 21474836;
-
 	private EV3IRSensor ir = new EV3IRSensor(SensorPort.S4); // use port S4 for IR Sensor
 	private SensorMode seek = ir.getSeekMode(); // initiate seekmode in Sensor
 	private float[] sample = new float[seek.sampleSize()]; // declare array to store samples form Sensor
 	private boolean beaconFound;
 	private int bearing;
 	private int distance;
+	private boolean stop = false;
+	private Pilot pilot = new Pilot();
 
-	private static Pilot pilot = new Pilot();
+	
 
 	public boolean isBeaconFound() {
 		return beaconFound;
@@ -35,64 +34,87 @@ public class BeaconFinder {
 		super();
 	}
 
-	/** @author loek (+frankl for thread)
-	 * When placed in area with beacon, it will roam until sensor will measure some
-	 * values. When inRange it should turn & drive towards beacon. 
-	 * NOTE: sensor is slow. Works, but it's not pretty
+	/**
+	 * @author loek (+frankl for thread) When placed in area with beacon, it will
+	 *         roam until sensor will measure some values. When inRange it should
+	 *         turn & drive towards beacon. NOTE: sensor is slow. Works, but it's
+	 *         not pretty
 	 */
 	public void findBeacon() {
-		while (Button.DOWN.isUp()) {
-			distance = fetchDistance();
-			System.out.println("1st while. Distance: " + distance);
-			Roamer roaming = new Roamer(beaconFound);
-			Thread roamThread = new Thread(roaming);
-			while (distance > 3 && Button.DOWN.isUp()) {
-				if (distance >= DISTANCE_TRESHOLD_ROAM) {
-					roamThread.start();
-				}
-				while (distance >= DISTANCE_TRESHOLD_ROAM && Button.DOWN.isUp()) {
-					seekRange();
-				}
-				 // TODO setStopRoaming should not get 'true' but beaconFound as argument (or it can be, right?)
-				roaming.setStopRoaming(true);
-				while (distance < DISTANCE_TRESHOLD_ROAM && Button.DOWN.isUp()) { // inRange --> turn and drive to beacon
-					inRange();
-				}
-			}
-		}
-		ir.close();
-	}
-
-/**
- * initiates turn&drive towards beacon, fetches new measurement after each move
- */
-	private void inRange() {
-		beaconFound = true;
-		bearing = fetchBearing();
+//		Roamer roaming = new Roamer(beaconFound);
+//		Thread roamThread = new Thread(roaming);
+//		roamThread.start();
 		distance = fetchDistance();
-		System.out.println("2. inRange | Distance: " + distance);
-		System.out.println("2. inRange | Bearing: " + bearing);
-		// turn with bearing, use Pilot
-		pilot.rotate(-bearing);
-		distance = fetchDistance();
-		// drive distance to beacon
-		pilot.travel(distance * 10);
-		// fetch another sample to test if movement was sufficient
-		distance = fetchDistance();
-		bearing = fetchBearing();
+//		System.out.println("Distance = " + distance);
+//		while (!stop || (distance<=0)) { 
+//			distance = fetchDistance();
+//			System.out.println("1st while. Distance: " + distance);;
+//			while (distance >= 1500 || distance <= 0) {
+//				bearing = fetchBearing();
+//				distance = fetchDistance();
+//				System.out.println("2. Roaming | Distance: " + distance);
+//				System.out.println("2. Roaming | Bearing: " + bearing);
+//			}
+//			beaconFound = true;
+//			roaming.setStopRoaming(beaconFound);
+//			roamThread.interrupt();
+		pilot.arc(5000, 45);
+		pilot.arc(-5000, -45);
+			while (distance < 1000 && distance > 5) { // inRange --> turn and drive to beacon
+				bearing = fetchBearing();
+				distance = fetchDistance();
+				System.out.println("2. inRange | Distance: " + distance);
+				System.out.println("2. inRange | Bearing: " + bearing); //these prints ARE printed.
+				// turn with bearing
+				pilot.rotate(-bearing);   //Pilot does not rotate with bearing. Possible due to thread interrupt.
+				distance = fetchDistance();
+				// drive distance to beacon
+				pilot.travel(distance * 8); //Pilot does not travel to beacon. Possible due to thread interrupt.
+				// fetch another sample to test if movement was sufficient
+				distance = fetchDistance();
+				bearing = fetchBearing();
+				}
+//			if (distance <= 5) {
+//				stop = true;
+//			}
+//			beaconFound = false;
+//			roaming.setStopRoaming(beaconFound);
+//			if (stop = true) {
+//				break;
+//			}
+			ir.close();	
 	}
 
 	/**
-	 * initiates roamingMode from Pilot. Fetches new distance 
+	 * initiates turn&drive towards beacon, fetches new measurement after each move
 	 */
-	private void seekRange() {
-		beaconFound = false;
-		bearing = fetchBearing();
-		distance = fetchDistance();
-		System.out.println("2. Roaming | Distance: " + distance);
-		System.out.println("2. Roaming | Bearing: " + bearing);
-		distance = fetchDistance();
-	}
+//	private void inRange() {
+//		beaconFound = true;
+//		bearing = fetchBearing();
+//		distance = fetchDistance();
+//		System.out.println("2. inRange | Distance: " + distance);
+//		System.out.println("2. inRange | Bearing: " + bearing);
+//		// turn with bearing, use Pilot
+//		pilot.rotate(-bearing);
+//		distance = fetchDistance();
+//		// drive distance to beacon
+//		pilot.travel(distance * 10);
+//		// fetch another sample to test if movement was sufficient
+//		distance = fetchDistance();
+//		bearing = fetchBearing();
+//	}
+//
+//	/**
+//	 * initiates roamingMode from Pilot. Fetches new distance
+//	 */
+//	private void seekRange() {
+//		beaconFound = false;
+//		bearing = fetchBearing();
+//		distance = fetchDistance();
+//		System.out.println("2. Roaming | Distance: " + distance);
+//		System.out.println("2. Roaming | Bearing: " + bearing);
+//		distance = fetchDistance();
+//	}
 
 	/*
 	 * @return bearing measurement from Sensor
@@ -112,7 +134,7 @@ public class BeaconFinder {
 		seek.fetchSample(sample, 0);
 		distance = (int) sample[1];
 		return distance;
-		
+
 	}
-	
+
 }
