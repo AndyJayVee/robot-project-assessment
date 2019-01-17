dd160424f13447d8634421005f9c68226b524ee5
Leo Snel <leo.snel@hva.nl>
2019-01-17 12:18:44 +0100

    updated constructor MarvinMover

--- C14-F/src/models/MarvinMover.java
+++ C14-F/src/models/MarvinMover.java
@@ -14,62 +14,64 @@ import lejos.utility.Delay;
 
 public class MarvinMover { // provides movement to BeaconFinder and MoveBeacon classes
 
-	static EV3LargeRegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);
-	static EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.D);
+	EV3LargeRegulatedMotor motorA;
+	EV3LargeRegulatedMotor motorD;
 
 	public MarvinMover() {
 		super();
+		this.motorA = new EV3LargeRegulatedMotor(MotorPort.A);
+		this.motorD = new EV3LargeRegulatedMotor(MotorPort.D);
 	}
 
 	public void turnRightOnBlack() {
 		motorA.setSpeed(-140);
-		motorB.setSpeed(100);
+		motorD.setSpeed(100);
 		motorA.backward();
-		motorB.forward();
+		motorD.forward();
 	}
 
 	public void turnLeftOnWhite() {
 		motorA.setSpeed(100);
-		motorB.setSpeed(-200);
+		motorD.setSpeed(-200);
 		motorA.forward();
-		motorB.backward();
+		motorD.backward();
 	}
 
 	public void driveStraightOnGrey() {
 		motorA.setSpeed(-200);
-		motorB.setSpeed(-200);
+		motorD.setSpeed(-200);
 		motorA.backward();
-		motorB.backward();
+		motorD.backward();
 	}
 
 	public void turn90DegreesClockwise() { // turns vehicle 90 degrees right
-		motorB.setSpeed(80);
+		motorD.setSpeed(80);
 		motorA.setSpeed(-300);
-		motorB.forward();
+		motorD.forward();
 		motorA.backward();
 		Delay.msDelay(1400);
 	}
 
 	public void turn90DegreesCounterClockwise() { // turns vehicle 90 degrees left
 		motorA.setSpeed(80);
-		motorB.setSpeed(-300);
+		motorD.setSpeed(-300);
 		motorA.forward();
-		motorB.backward();
+		motorD.backward();
 		Delay.msDelay(1400);
 	}
 
 	public void turn180DegreesCounterClockwise() { // turns vehicle 180 degrees counter clockwise
 		motorA.setSpeed(80);
-		motorB.setSpeed(-300);
+		motorD.setSpeed(-300);
 		motorA.forward();
-		motorB.backward();
+		motorD.backward();
 		Delay.msDelay(2600);
 	}
 	
 	public void turn180DegreesClockwise() { // turns vehicle 180 degrees clockwise
-		motorB.setSpeed(80);
+		motorD.setSpeed(80);
 		motorA.setSpeed(-300);
-		motorB.forward();
+		motorD.forward();
 		motorA.backward();
 		Delay.msDelay(2600);
 	}
@@ -77,30 +79,30 @@ public class MarvinMover { // provides movement to BeaconFinder and MoveBeacon c
 	public void shortLeg() { // drives vehicle forward by approximately 20 cm
 		motorA.setSpeed(400);
 		motorA.forward();
-		motorB.setSpeed(400);
-		motorB.forward();
+		motorD.setSpeed(400);
+		motorD.forward();
 		Delay.msDelay(1400);
 	}
 	
 	public void shortBack() { // drives vehicle backward by approximately 20 cm
 		motorA.setSpeed(-400);
 		motorA.forward();
-		motorB.setSpeed(-400);
-		motorB.forward();
+		motorD.setSpeed(-400);
+		motorD.forward();
 		Delay.msDelay(700);
 	}
 
 	public void longLeg() { // drives vehicle forward by approximately 40 cm
 		motorA.setSpeed(400);
 		motorA.forward();
-		motorB.setSpeed(400);
-		motorB.forward();
+		motorD.setSpeed(400);
+		motorD.forward();
 		Delay.msDelay(2600);
 	}
 	
 	public void stopMoving() { // stops both motors
 		motorA.stop();
-		motorB.stop();
+		motorD.stop();
 	}
 
 }
