6bbd4bac53eeeb10922d85a918bcce1352e7f1e9
Andy Valkenburg <andy.valkenburg@hva.nl>
2019-01-17 10:52:42 +0100

    last changes in menu and GameLauncher

--- C14-F/src/main/GameLauncher.java
+++ C14-F/src/main/GameLauncher.java
@@ -22,26 +22,36 @@ public class GameLauncher {
 		String currentGame;
 		Menu menu = new Menu();
 		boolean repeatAfterfinish = true;
-		while (repeatAfterfinish) {
-			currentGame = menu.chooseGame();
 
-			if (currentGame.equals("linefollower")) {
-				LineFollower lineFollower = new LineFollower();
-				lineFollower.followLine();
-				currentGame = menu.gameStopped("Line follower");
-			} else if (currentGame.equals("beaconfinder")) {
-				BeaconFinder beaconFinder = new BeaconFinder();
-				beaconFinder.findBeacon();
-				currentGame = menu.gameStopped("Beacon finder");
-			} else if (currentGame.equals("movebeacon")) {
-				MoveBeacon moveBeacon = new MoveBeacon();
-				moveBeacon.grabBeacon();
-				currentGame = menu.gameStopped("Move beacon");
-			} else if (currentGame.equals("nogame")) {
-				currentGame = menu.gameStopped("All programs");
-			} else
-				repeatAfterfinish = false;
-
+		try {
+			while (repeatAfterfinish) {
+				currentGame = menu.chooseGame();
+				if (currentGame.equals("linefollower")) {
+					LineFollower lineFollower = new LineFollower();
+					lineFollower.followLine();
+//				                               "123456789987654321" ruler comment
+					currentGame = menu.gameStopped("   Line follower  ");
+				} else if (currentGame.equals("beaconfinder")) {
+					BeaconFinder beaconFinder = new BeaconFinder();
+					beaconFinder.findBeacon();
+//                                             "123456789987654321" ruler comment
+					currentGame = menu.gameStopped("   Beacon finder  ");
+				} else if (currentGame.equals("movebeacon")) {
+					MoveBeacon moveBeacon = new MoveBeacon();
+					moveBeacon.grabBeacon();
+//                                             "123456789987654321" ruler comment
+					currentGame = menu.gameStopped("    Move beacon   ");
+				} else if (currentGame.equals("nogame")) {
+					currentGame = menu.gameStopped("All programs");
+				}
+				if (currentGame.equals("repeat")) {
+					repeatAfterfinish = true;
+				} else
+					repeatAfterfinish = false;
+			}
+		} catch (Exception e) {
+			menu.gameStopped("   Error occured, ");
 		}
+
 	}
 }
